9d2b4cdf1e61a7a1838638b253bcd33e08300706
Mubarak <adeshinamubarak6.com>
2023-03-09 12:52:27 +0100

    building the complete landing page

---
+++ src/components/Footer.jsx
@@ -0,0 +1,198 @@
+import React from "react";
+import styled from "styled-components";
+
+function Footer(){
+return(
+    <Wrapper className="">
+        <div className="one">
+        <div className="page-center">
+          <div >
+            <div className="all-first">
+                <div className="first">
+                    <ul>
+                        <li><h3><button>Logo</button></h3></li>
+                        <p>Streamline your financials - The ultimate platform for freelancers and salaried workers. Effortlessly manage your international payments. </p>
+                    </ul>
+                </div>
+                <div className="first1">
+                    <ul>
+                    <h3>products</h3>
+                    <li>Savings</li>
+                    <li>Send</li>
+                    <li>Receive</li>
+                    <li>Domain Account</li>
+                    </ul>
+                </div>
+                <div className="first2">
+                    <ul>
+                    <h3>About</h3>
+                    <li>About Us</li>
+                    <li>Contact</li>
+                    <li>Team</li>
+                    <li>Partner</li>
+                    </ul>
+                </div>
+                <div className="first3">
+                    <ul>
+                    <h3>Resources</h3>
+                    <li>Document</li>
+                    <li>Blog</li>
+                    <li>Career</li>
+                    <li>FAQ</li>
+                    </ul>
+                </div>
+                </div>
+            <div className="foot">
+            <p>terms & conditions          |          privacy & policy</p>
+        </div>
+            </div>
+        
+
+        <div className="border">
+        <div className="bord"><h3>2023 copyright by matmosPay. All right reserved.</h3></div>
+        <div className="bords">
+        <img src={process.env.PUBLIC_URL + '/Mubarak img/instagram.png'} alt="main"  />
+        <img src={process.env.PUBLIC_URL + '/Mubarak img/twitter.png'} alt="main"  />
+        <img src={process.env.PUBLIC_URL + '/Mubarak img/linkedin.png'} alt="main"  />
+        <img src={process.env.PUBLIC_URL + '/Mubarak img/facebook.png'} alt="main"  />
+            
+        </div>
+        </div>
+
+        </div>
+     </div>
+
+    </Wrapper>
+)
+
+
+}
+const Wrapper = styled.article `
+    
+
+    .one{
+        background-color: black;
+        min-height: 70vh;
+        margin-top: 100px;
+        color: white;
+        width: 100%;
+        display: flex;
+        flex-direction: column;
+        justify-content: center;
+        align-items: center;
+    }
+    .all-first{
+        display: flex;
+        align-items: center;
+        flex-direction: row;
+      
+    }
+    .all-first li{
+        line-height: 2.7rem;
+        cursor: pointer;
+    }
+    .all-first li:hover{
+        color: #7ED07E;
+    }
+    .all-first p{
+        margin-top: 1rem;
+        line-height: 2.2rem;
+    }
+    .all-first h3{
+        color: #90EE90;
+    }
+    .foot{
+    border-bottom: 3px solid #7ED07E;
+    color: #90EE90;
+     width: 100%;
+     cursor: pointer;
+     font-size: 20px;
+     display: flex;
+     justify-content: center;
+     flex-direction: row;
+     height: 50px;
+     padding-left: 80px;
+    }
+    .first button{
+        background-color: #90EE90;
+        padding: 10px 40px;
+        font-size: 20px;
+        cursor: pointer;
+    }
+    .first{
+        width: 40%;
+    }
+    .first p{
+        width: 70%;
+    }
+    
+    .first1{
+        width: 20%;
+    }
+    .first2{
+        width: 20%;
+    }
+    .first3{
+        width: 20%;
+    }
+    .border{
+        margin-top: 50px;
+        display: flex;
+        flex-direction: row;
+        align-items: flex-;
+        justify-content: space-between;
+    }
+    .bord{
+        /* margin-top: 50px; */
+    }
+    .bords{
+        /* margin-top: 50px; */
+        display: flex;
+        flex-direction: row;
+        gap: 20px;
+    }
+    overflow: hidden;
+    @media screen and (max-width:768px) {
+        .all-first{
+        display: flex;
+        align-items: center;
+        flex-direction: column;
+      
+    }        
+    .first{
+        width: 100%;
+    }
+    .first p{
+        width: 100%;
+    }
+    
+    .first1{
+        width: 100%;
+    }
+    .first2{
+        width: 100%;
+    }
+    .first3{
+        width: 100%;
+    }
+    .all-first button{
+        margin-top: 30px;
+    }
+    .bords{
+        margin-top: 50px;
+        display: flex;
+        flex-direction: row;
+        gap: 10px;
+        width: 100%;
+    }
+    .border{
+        margin-top: 50px;
+        display: flex;
+        flex-direction: column;
+        align-items: flex-;
+        justify-content: space-between;
+    }
+    } 
+`
+
+export default Footer;
---
+++ src/components/Help.js
@@ -0,0 +1,138 @@
+import React from 'react'
+import styled from 'styled-components';
+function Help() {
+  return (
+   <Wrapper className='page-center'>
+   <div className="one">
+    <h2>How Can We Help You?</h2>
+   </div>
+   <div className="three">
+   <div className="two">
+    <h1>Contact Us</h1>
+    <p>If you have any questions or need assistance with our platform, please don't hesitate to contact us. You can reach us through our customer support team, by email, phone or live chat.</p>
+    <button>Frequently Asked Questions</button>
+   </div>
+   <div className="twoo">
+        <div className="twooo">
+        <input type="text" placeholder='Your Name' />
+        <input type="email" placeholder='Your Email' required />
+        </div>
+        
+        <textarea name="" id="" cols="50" rows="10" placeholder='Message'></textarea>
+        <button id='Send'>Send Us Message</button>
+    </div>
+      
+   </div>
+   </Wrapper>
+ )
+}
+ const Wrapper = styled.section`
+    .one h2{
+        text-align: center;
+        margin-top: 50px;
+    }
+    .two{
+        width: 100%;
+    }
+    .two p{
+        width: 70%;
+        font-size: 20px;
+        margin-top: 2rem;
+    }
+    .twooo{
+        width: 100%;
+        display: flex;
+        gap: 20px;
+        flex-direction: row;
+    }
+    #Send{
+        color: #75C175;
+        background-color: black;
+    }
+    .twoo input{
+        background-color: #F7F7F7;
+        padding: 1px 40px;
+        height  :5vh ;
+        border: none;
+        border-radius: 10px;
+        outline-color: #75C175;
+    } 
+    .twoo textarea{
+        display: flex;
+        background-color:  #F7F7F7;
+        align-items: center;
+        justify-content: center;
+        width: 100%;
+        padding: 20px;
+        border: none;
+        border-radius: 10px;
+        margin-top: 20px;
+        outline-color: #75C175;
+    }
+    .twoo textarea::placeholder{
+    /* padding: 20px; */
+    }
+    .two input:hover{
+        border: none;
+    }
+    .three{
+        display: flex;
+        margin-top: 100px;
+        flex-direction: row;
+    }
+    .three button{
+        background-color: #F7F7F7;
+        padding: 15px 50px;
+        border-radius: 10px;
+        border: none;
+        box-shadow: 2px 2px 4px rgba(0, 0, 0, 0.2);
+        margin-top: 50px;
+    }
+    .two button{
+        background-color: #F7F7F7;
+        font-size: 15px;
+    }
+    .two button:hover{
+        background-color: black;
+        color: #F7F7F7;
+    }
+    .row {
+        display: flex;
+        flex-direction: column;
+    }
+   .twoo input{
+  background-color: #F7F7F7;
+  /* padding: 1px 40px; */
+  height  :5vh ;
+  border: none;
+  border-radius: 10px;
+}
+
+    @media screen and (max-width:768px) {
+        .three{
+        display: flex;
+        margin-top: 100px;
+        flex-direction: column;
+    }
+    .two p{
+        width: 100%;
+        font-size: 20px;
+        margin-top: 2rem;
+    }
+    .twooo{
+        width: 100%;
+        display: flex;
+        gap: 20px;
+        margin-top: 50px;
+        flex-direction: row;
+    }
+    .twooo{
+        width: 100%;
+        display: flex;
+        gap: 20px;
+        flex-direction: column;
+    }
+    }
+ `
+
+export default Help;
