3012903e483e92c2625a3f0c3d2dc7d45990811d
Ayanda-bolwatife <ayandastephen1000@gmail.com>
2023-04-28 13:20:51 +0100

    reduce padding

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -209,7 +209,7 @@ const Wrapper = styled.article `
     }
     .first3{
         width: 100%;
-        padding-left: 100px;
+        padding-left: 80px;
     }
     .all-first button{
         margin-top: 30px;
